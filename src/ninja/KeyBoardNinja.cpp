d73ae6bcb7d3d900e41121cfddc19a89fe9d6856
Voropaev Kirill <2vvoropaevv@gmail.com>
2021-05-05 23:18:48 +0700

    Code refactoring into function

--- src/ninja/KeyBoardNinja.cpp
+++ src/ninja/KeyBoardNinja.cpp
@@ -1,148 +1,12 @@
 #include <SFML/Graphics.hpp>
 
-using namespace sf; // что бы не писать постоянно sf::
+#include "ninjalib/helper.h"
+#include "ninjalib/main_menu.h"
+ // что бы не писать постоянно sf::
 
-int main_menu(RenderWindow& window, int& difficulty)
-{ // Функция главного меню
-    Texture tex_start, tex_about, tex_show_score, tex_exit, tex_bg, tex_rules,
-            tex_diff, tex_ShowScore;
-
-    tex_start.loadFromFile("images/Start_menu.png");
-    tex_about.loadFromFile("images/About_menu.png");
-    tex_show_score.loadFromFile("images/Hight_score_menu.png");
-    tex_exit.loadFromFile("images/Exit_menu.png");
-    tex_bg.loadFromFile("images/Background_menu.jpg");
-    tex_rules.loadFromFile("images/Rules.jpg");
-    tex_diff.loadFromFile("images/Difficulty.jpg");
-    tex_ShowScore.loadFromFile("images/ShowScore.jpg");
-
-    Sprite start(tex_start), about(tex_about), show_score(tex_show_score),
-            exit(tex_exit), bg(tex_bg), rules(tex_rules), diff(tex_diff),
-            ShowScore(tex_ShowScore);
-
-    show_score.setPosition(30, 14);
-    start.setPosition(100, 105);
-    about.setPosition(150, 200);
-    exit.setPosition(150, 307);
-    rules.setPosition(637, 0.5);
-    diff.setPosition(0, 0);
-    ShowScore.setPosition(0, 0);
-
-    int select = 0, isMenu = 1, ShowRules = 0, ShowDiff = 0, ShowScores = 0;
-
-    Time delayTime = sf::milliseconds(100);
-
-    while (isMenu) {
-        Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == Event::Closed)
-                window.close();
-        }
-
-        show_score.setColor(Color::White);
-        start.setColor(Color::White);
-        about.setColor(Color::White);
-        exit.setColor(Color::White);
-
-        select = 0;
-
-        if (!ShowDiff && !ShowScores) {
-            if (IntRect(31, 19, 490, 60).contains(Mouse::getPosition(window))) {
-                show_score.setColor(Color::Red);
-                select = 1;
-            }
-            if (IntRect(100, 110, 350, 60)
-                        .contains(Mouse::getPosition(window))) {
-                start.setColor(Color::Red);
-                select = 2;
-            }
-            if (IntRect(180, 217, 170, 50)
-                        .contains(Mouse::getPosition(window))) {
-                about.setColor(Color::Red);
-                select = 3;
-            }
-            if (IntRect(200, 315, 130, 55)
-                        .contains(Mouse::getPosition(window))) {
-                exit.setColor(Color::Red);
-                select = 4;
-            }
-        }
-
-        if (Mouse::isButtonPressed(Mouse::Left)) {
-            if (select == 1) {
-                if (!ShowScores)
-                    ShowScores = 1;
-                else
-                    ShowScores = 0;
-                sleep(delayTime);
-            }
-            if (select == 2) {
-                if (!ShowDiff)
-                    ShowDiff = 1;
-                else
-                    ShowDiff = 0;
-                sleep(delayTime);
-            }
-            if (select == 3) {
-                if (!ShowRules)
-                    ShowRules = 1;
-                else
-                    ShowRules = 0;
-                sleep(delayTime);
-            }
-            if (select == 4) {
-                return 0;
-            }
-        }
-
-        window.draw(bg);
-        window.draw(start);
-        window.draw(about);
-        window.draw(exit);
-        window.draw(show_score);
-        if (ShowRules)
-            window.draw(rules);
-
-        if (ShowScores) {
-            window.draw(ShowScore);
-            if (IntRect(315, 826, 490, 50).contains(Mouse::getPosition(window))
-                && Mouse::isButtonPressed(Mouse::Left))
-                ShowScores = 0;
-        }
-
-        if (ShowDiff) {
-            window.draw(diff);
-            if (IntRect(396, 807, 288, 80).contains(Mouse::getPosition(window))
-                && Mouse::isButtonPressed(Mouse::Left))
-                ShowDiff = 0;
-            else if (
-                    IntRect(373, 349, 217, 257)
-                            .contains(Mouse::getPosition(window))
-                    && Mouse::isButtonPressed(Mouse::Left)) {
-                difficulty = 1;
-                isMenu = false;
-            } else if (
-                    IntRect(667, 355, 210, 247)
-                            .contains(Mouse::getPosition(window))
-                    && Mouse::isButtonPressed(Mouse::Left)) {
-                difficulty = 2;
-                isMenu = false;
-            } else if (
-                    IntRect(954, 352, 204, 286)
-                            .contains(Mouse::getPosition(window))
-                    && Mouse::isButtonPressed(Mouse::Left)) {
-                difficulty = 3;
-                isMenu = false;
-            }
-        }
-        window.display();
-    }
-    return 1;
-}
-
-void Generate_letter(Sprite& letter)
+void Generate_letter(sf::Sprite& letter)
 { // Выдать спрайту рандомную букву
-
+    using namespace sf;
     int Letters[26][3], kind = 0;
     int i;
     int x = 28, y = 9;
@@ -163,6 +27,7 @@ void Generate_letter(Sprite& letter)
 
 int main()
 {
+    using namespace sf;
     RenderWindow window(VideoMode(1536, 960), "KeyBoardNinja");
     Texture tex_pause, tex_hp, tex_game_bg, tex_menu_pause, tex_letters;
     tex_pause.loadFromFile("images/pause.png");
