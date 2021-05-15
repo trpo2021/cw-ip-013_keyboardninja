f0d19aa3031945a66cd9d5296a23b23ad109a453
Voropaev Kirill <2vvoropaevv2@gmail.com>
2021-05-15 22:21:10 +0700

    Use clang config

--- src/ninjalib/main_menu.cpp
+++ src/ninjalib/main_menu.cpp
@@ -1,62 +1,64 @@
+#include "Select.h"
 #include "helper.h"
 #include <SFML/Graphics.hpp>
-#include "Select.h"
 
-int main_menu(sf::RenderWindow& window, Difficult& difficult, std::vector <My_Sprite*> &static_spr_mas)
+int main_menu(
+        sf::RenderWindow& window,
+        Difficult& difficult,
+        std::vector<My_Sprite*>& static_spr_mas)
 { // Функция главного меню
-	using namespace sf;
+    using namespace sf;
 
-	int select = 0;
-	int isMenu = 1, ShowRules = 0, ShowDiff = 0, ShowScores = 0;
+    int select = 0;
+    int isMenu = 1, ShowRules = 0, ShowDiff = 0, ShowScores = 0;
 
+    while (isMenu) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed)
+                window.close();
+        }
 
+        static_spr_mas[SPR_SCORE_MENU]->Get_sprite().setColor(Color::White);
+        static_spr_mas[SPR_START]->Get_sprite().setColor(Color::White);
+        static_spr_mas[SPR_ABOUT]->Get_sprite().setColor(Color::White);
+        static_spr_mas[SPR_EXIT]->Get_sprite().setColor(Color::White);
 
-	while (isMenu) {
-		Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == Event::Closed)
-				window.close();
-		}
+        select = 0;
 
-		static_spr_mas[SPR_SCORE_MENU]->Get_sprite().setColor(Color::White);
-		static_spr_mas[SPR_START]->Get_sprite().setColor(Color::White);
-		static_spr_mas[SPR_ABOUT]->Get_sprite().setColor(Color::White);
-		static_spr_mas[SPR_EXIT]->Get_sprite().setColor(Color::White);
+        if (!ShowDiff && !ShowScores) {
+            if (IntRect(31, 19, 490, 60).contains(Mouse::getPosition(window))) {
+                static_spr_mas[SPR_SCORE_MENU]->Get_sprite().setColor(
+                        Color::Red);
+                select = 1;
+            }
+            if (IntRect(100, 110, 350, 60)
+                        .contains(Mouse::getPosition(window))) {
+                static_spr_mas[SPR_START]->Get_sprite().setColor(Color::Red);
+                select = 2;
+            }
+            if (IntRect(180, 217, 170, 50)
+                        .contains(Mouse::getPosition(window))) {
+                static_spr_mas[SPR_ABOUT]->Get_sprite().setColor(Color::Red);
+                select = 3;
+            }
+            if (IntRect(200, 315, 130, 55)
+                        .contains(Mouse::getPosition(window))) {
+                static_spr_mas[SPR_EXIT]->Get_sprite().setColor(Color::Red);
+                select = 4;
+            }
+        }
 
-		select = 0;
+        if (Mouse::isButtonPressed(Mouse::Left)) {
+            if (!Select(select, ShowDiff, ShowRules, ShowScores))
+                return 0;
+        }
 
-		if (!ShowDiff && !ShowScores) {
-			if (IntRect(31, 19, 490, 60).contains(Mouse::getPosition(window))) {
-				static_spr_mas[SPR_SCORE_MENU]->Get_sprite().setColor(Color::Red);
-				select = 1;
-			}
-			if (IntRect(100, 110, 350, 60)
-				.contains(Mouse::getPosition(window))) {
-				static_spr_mas[SPR_START]->Get_sprite().setColor(Color::Red);
-				select = 2;
-			}
-			if (IntRect(180, 217, 170, 50)
-				.contains(Mouse::getPosition(window))) {
-				static_spr_mas[SPR_ABOUT]->Get_sprite().setColor(Color::Red);
-				select = 3;
-			}
-			if (IntRect(200, 315, 130, 55)
-				.contains(Mouse::getPosition(window))) {
-				static_spr_mas[SPR_EXIT]->Get_sprite().setColor(Color::Red);
-				select = 4;
-			}
-		}
-
-		if (Mouse::isButtonPressed(Mouse::Left)) {
-			if (!Select(select, ShowDiff, ShowRules, ShowScores)) return 0;
-		}
-
-
-		window.draw(static_spr_mas[SPR_BG]->Get_sprite());
-		window.draw(static_spr_mas[SPR_START]->Get_sprite());
-		window.draw(static_spr_mas[SPR_ABOUT]->Get_sprite());
-		window.draw(static_spr_mas[SPR_EXIT]->Get_sprite());
-		window.draw(static_spr_mas[SPR_SCORE_MENU]->Get_sprite());
+        window.draw(static_spr_mas[SPR_BG]->Get_sprite());
+        window.draw(static_spr_mas[SPR_START]->Get_sprite());
+        window.draw(static_spr_mas[SPR_ABOUT]->Get_sprite());
+        window.draw(static_spr_mas[SPR_EXIT]->Get_sprite());
+        window.draw(static_spr_mas[SPR_SCORE_MENU]->Get_sprite());
 
         if (ShowDiff) {
             window.draw(static_spr_mas[SPR_DIFF]->Get_sprite());
